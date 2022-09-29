f3370f6fd5a0e235957acb97e5d0b426454007a7
terminaate <tntwnik@gmail.com>
2022-09-29 07:17:42 +0300

    Initial commit of combining electron + react

---
+++ src/client/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import {join as pJoin} from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: pJoin(__dirname, "../../dist/client"),
+    emptyOutDir: true
+  },
+  base: ""
+})
